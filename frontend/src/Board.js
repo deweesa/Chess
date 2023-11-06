bbab4384a5d3e8222debbacb9fa51f20018fe1fb
deweesa <asadeweese@gmail.com>
2023-11-05 16:20:29 -0800

    Added ValidMoves state and css class indicator
    Added a 2d array to hold valid moves for the picked up piece. Using a
    simple circle as the place holder for squares that are a valid move.
    
    Right now we're just setting the (3,3) position for any piece that is
    picked up. Will work on generating actual valid moves next.

--- frontend/src/Board.js
+++ frontend/src/Board.js
@@ -10,7 +10,10 @@ export default function Board() {
   const [counter, setCounter] = useState(0);
   const [inHand, setInHand] = useState(false);
   const [coords, setCoords] = useState([0, 0]);
+  const [isWhiteTurn, setTurn] = useState(true);
+  const [validMoves, setValidMoves] = useState([]);
 
+//  validMoves.add({row: 3, col: 3})
   useEffect(() => {
     const result = [];
 
@@ -23,13 +26,23 @@ export default function Board() {
     result.push(constants.W_PAWN_ROW);
     result.push(constants.W_KING_ROW);
 
+    const initValidMoves = []
+    for(let i = 0; i < rows; i++) {
+      const row = new Array(cols).fill(0);
+      initValidMoves.push(row);
+    } 
+
+    setValidMoves(initValidMoves);
     setChessBoard(result);
   }, [])
 
   const handleClick = (r, c) => {
     //todo: this wokrs perfectly as long as you only click a piece, it's location
     //      and then another piece. If you start clicking "out of order" it's borked
-    if(chessBoard[r][c] === constants.EMPTY && !inHand) return;
+    validMoves[3][3] = 1;
+    setValidMoves(validMoves);
+    let clickedLocation = chessBoard[r][c]
+    if(clickedLocation === constants.EMPTY && !inHand) return;
 
     if(inHand) {
       chessBoard[r][c] = chessBoard[coords[0]][coords[1]];
@@ -59,17 +72,18 @@ export default function Board() {
                     }`}
                     key={cIndex}
                     onClick={() => handleClick(rIndex, cIndex)}
-                    rIndex={rIndex}
-                    cIndex={cIndex}
                   >
-                    {rIndex}, {cIndex}: {chessBoard[rIndex][cIndex]}
+                    <div>{rIndex}, {cIndex}: {chessBoard[rIndex][cIndex]}</div>
+                    {validMoves[rIndex][cIndex]? <div className="circle"/> : ""}
                   </div>
                 )
               })}
             </div>)
           })}
       </div>
-      
+      <div>
+        Player Turn: {isWhiteTurn? "White" : "Black"}
+      </div>
     </>
   )
 }
